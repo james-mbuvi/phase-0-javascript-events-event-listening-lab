f4ac3538c1e962d06d79d3f9e2cf0631990b8779
JAMES MBUVI <completed assignment>
2023-12-23 10:24:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-function addingEventListener() {
-}
+// index.js
+document.addEventListener('DOMContentLoaded', function () {
+  // Wait for the DOM to be fully loaded before binding the event listener
+  const myButton = document.getElementById('myButton');
+
+  if (myButton) {
+    // Define the event listener function
+    function handleClick() {
+      console.log('Button clicked!');
+    }
+
+    // Bind the event listener to the button
+    myButton.addEventListener('click', handleClick);
+  } else {
+    console.error('Button with ID "myButton" not found.');
+  }
+});
+
