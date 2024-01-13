f26d7fdfa93133c52e25cdcb7456a00dbde01564
JAMES MBUVI <completed assignment>
2024-01-13 09:23:01 +0300

    completed addignment

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
   if (myButton) {
     // Define the event listener function
+    const input = document.getElementById('button')
+
     function handleClick() {
       console.log('Button clicked!');
     }
 
     // Bind the event listener to the button
+    function addingEventListener() {
+      const input = document.getElementById("button");
+      input.addEventListener("click", function () {
+        alert("I was clicked!");
+      });
+    }
+input.addEventListener('click', clickAlert);
     myButton.addEventListener('click', handleClick);
   } else {
     console.error('Button with ID "myButton" not found.');
